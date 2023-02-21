8b8b7fe1ffc58ab3119090be5ce24360c0c0f5ec
Yakub Ali <mamuzaan@yahoo.com>
2023-02-21 22:45:26 +0000

    Added category by filte

--- products/views.py
+++ products/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 from django.views import generic, View
 
 
 def all_products(request):
     products = Product.objects.all()
     query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     if request.GET:
         if 'q' in request.GET:
@@ -20,7 +27,7 @@ def all_products(request):
             products = products.filter(queries)
 
     return render(
-        request, "products/products.html", {'products': products,'search_term': query})
+        request, "products/products.html", {'products': products,'search_term': query, 'current_categories': categories})
 
 
 def product_detail(request, slug):
