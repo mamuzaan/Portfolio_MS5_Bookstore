35dae4214fcc80d7b9ada4a7e3eb3b0dc9ef9959
Yakub Ali <mamuzaan@yahoo.com>
2023-02-21 10:34:03 +0000

    Added category models in the products app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+
+    def __str__(self):
+        return self.name
+
