547d5a95c3cbfb4544e7c6a5d4bee065d525dffa
Yakub Ali <mamuzaan@yahoo.com>
2023-02-22 12:31:56 +0000

    Added add product to bag functionality add_to_basket, url, and add form to product details template

--- basket/context_processors.py
+++ basket/context_processors.py
@@ -1,10 +1,9 @@
-from .basket import Basket
 from decimal import Decimal
 from django.conf import settings
 
 
-def view_basket(request):
-    bag_items = []
+def basket(request):
+    basket_items = []
     total = 0
     product_count = 0
 
@@ -14,9 +13,9 @@ def view_basket(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
-    
+
     context = {
         'basket_items': basket_items,
         'total': total,
--- basket/urls.py
+++ basket/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'basket'
 
 urlpatterns = [
-    path('', views.view_basket, name='view_basket'),
+    path('', views.basket, name='basket'),
+    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
 ]
