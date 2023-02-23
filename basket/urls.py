492cf20a0b21ab2596ff51ccd1a4ca5685645eca
Yakub Ali <mamuzaan@yahoo.com>
2023-02-23 01:56:47 +0000

    Added adjust_busket function and add to url for update quantity functionality in basket

--- basket/urls.py
+++ basket/urls.py
@@ -6,4 +6,5 @@ app_name = 'basket'
 urlpatterns = [
     path('', views.basket, name='basket'),
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('adjust/<item_id>/', views.adjust_basket, name='adjust_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 def basket(request):
@@ -20,3 +20,18 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('basket:basket'))
