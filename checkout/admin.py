b4826577b86d2db2221ac8964df45e8cde221405
Yakub Ali <mamuzaan@yahoo.com>
2023-02-24 14:50:53 +0000

    Added class OrderLineItemInLine to checkout apps admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+admin.site.register(Order, OrderAdmin)
