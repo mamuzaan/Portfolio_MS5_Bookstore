9a902c0aaaaab554d676dfd1dbed074e6e6ec020
Yakub Ali <mamuzaan@yahoo.com>
2023-02-24 14:57:00 +0000

    Added OrderAdmin class for checkout apps Order model and added admin register

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,9 +2,27 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 
 
+@admin.register(OrderLineItem)
 class OrderLineItemInline(admin.TabularInline):
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
 
 
-admin.site.register(Order, OrderAdmin)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemInline,)
+    readonly_fields = ('order_number', 'date', 'order_total_price', 'grand_total_price',)
+    list_display = ('order_number', 'date', 'name', 'email', 'phone_number', 'order_total_price', 'delivery_cost', 'grand_total_price',)
+    ordering = ('-date',)
+
+    fieldsets = (
+        ('Order Information', {
+            'fields': ('order_number', 'user', 'name', 'email', 'phone_number', 'date',)
+        }),
+        ('Delivery Address', {
+            'fields': ('street_address1', 'town_or_city', 'postcode', 'county', 'country',)
+        }),
+        ('Order Totals', {
+            'fields': ('order_total_price', 'delivery_cost', 'grand_total_price',)
+        }),
+    )
