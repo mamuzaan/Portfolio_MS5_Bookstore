6d2c6ee536388210ec0064f203c6e0e22387888e
Yakub Ali <mamuzaan@yahoo.com>
2023-02-25 13:57:31 +0000

    Added stripe payment intent, grand total via basket function

--- checkout/views.py
+++ checkout/views.py
@@ -7,21 +7,33 @@ import stripe
 
 
 def checkout(request):
-    basket = request.session.get('basket', {})
-    if not basket:
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    basket_items = request.session.get('basket', {})
+    if not basket_items:
         messages.error(request, "There's nothing in your basket at the moment")
         return redirect(reverse('products'))
 
     current_basket = basket(request)
     total = current_basket['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \ Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51MNIpmBelBamBv6rMu11wsjz28GfGBs2iFBarpXeyENjNwQ3VxQ1nkFktzQyPXeYT3dJQVEQ2SwbwT5OjoUkKKaX00YwOQ9kH2',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
