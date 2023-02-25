53abe806a6f95aa54c53dc8c9c097c5fecac1eac
Yakub Ali <mamuzaan@yahoo.com>
2023-02-25 00:51:31 +0000

    Added card element form to checkout template and import js code from stripe and added css

---
+++ static/checkout/js/stripe_elements.js
@@ -0,0 +1,26 @@
+// Set your publishable key: remember to change this to your live publishable key in production
+// See your keys here: https://dashboard.stripe.com/apikeys
+
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+
+// Set up Stripe.js and Elements to use in checkout form
+var elements = stripe.elements();
+var style = {
+  base: {
+    color: "#32325d",
+  }
+};
+
+var card = elements.create("card", { style: style });
+card.mount("#card-element");
+
+card.on('change', ({error}) => {
+    let displayError = document.getElementById('card-errors');
+    if (error) {
+      displayError.textContent = error.message;
+    } else {
+      displayError.textContent = '';
+    }
+  });
