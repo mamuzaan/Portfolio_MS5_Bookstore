608331f98489e91a064c7e0ce59801188a590781
Yakub Ali <mamuzaan@yahoo.com>
2023-02-25 14:34:12 +0000

    Added handle form submit functionality

--- static/checkout/js/stripe_elements.js
+++ static/checkout/js/stripe_elements.js
@@ -1,9 +1,9 @@
 // Set your publishable key: remember to change this to your live publishable key in production
 // See your keys here: https://dashboard.stripe.com/apikeys
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 
 // Set up Stripe.js and Elements to use in checkout form
 var elements = stripe.elements();
@@ -24,3 +24,33 @@ card.on('change', ({error}) => {
       displayError.textContent = '';
     }
   });
+
+// Handle form submit
+const form = document.getElementById('payment-form');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  card.update({ 'disabled': True});
+  $('#submit-button').attr('disabled', true)
+
+  const {error} = await stripe.confirmPayment({
+    //`Elements` instance that was used to create the Payment Element
+    elements,
+    confirmParams: {
+      return_url: 'https://example.com/order/123/complete',
+    },
+  });
+
+  if (error) {
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Show error to your customer (for example, payment
+    // details incomplete)
+    let displayError = document.getElementById('card-errors');
+    const messageContainer = document.querySelector('#error-message');
+    messageContainer.textContent = error.message;
+    card.update({ 'disabled': True});
+  $('#submit-button').attr('disabled', true)
+  } else {
+    form.submit();
+  }
+});
