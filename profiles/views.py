360b761178745fda16bc42e2b44c60b27cbd5069
Yakub Ali <mamuzaan@yahoo.com>
2023-02-28 09:16:45 +0000

    Added form and orders var to profiles views

--- profiles/views.py
+++ profiles/views.py
@@ -6,9 +6,13 @@ def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserProfileForm(request.POST, instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
