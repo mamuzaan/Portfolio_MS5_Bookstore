a722a0da94e82ca2c48da03544692b5d0229fd9e
Yakub Ali <mamuzaan@yahoo.com>
2023-03-05 20:04:14 +0000

    fix line is too long products model

--- products/models.py
+++ products/models.py
@@ -8,22 +8,22 @@ class Category(models.Model):
     slug = models.SlugField(max_length=255, unique=True)
 
     class Meta:
-        verbose_name_plural = 'categories'
+        verbose_name_plural = "categories"
 
     def get_absolute_url(self):
-        return reverse('products:category_list', args=[self.slug])
+        return reverse("products:category_list", args=[self.slug])
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    category = models.ForeignKey(Category, related_name="product", on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="product_creator")
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255, default='admin')
+    author = models.CharField(max_length=255, default="admin")
     description = models.TextField(blank=True)
-    image = models.ImageField(null=True, blank=True, default='default.png')
+    image = models.ImageField(null=True, blank=True, default="default.png")
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
@@ -33,11 +33,11 @@ class Product(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name_plural = 'Products'
-        ordering = ('-created',)
+        verbose_name_plural = "Products"
+        ordering = ("-created",)
 
     def get_absolute_url(self):
-        return reverse('products:product_detail', args=[self.slug])
+        return reverse("products:product_detail", args=[self.slug])
 
     def __str__(self):
         return self.title
